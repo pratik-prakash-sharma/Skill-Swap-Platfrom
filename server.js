e8466ec7282a68005275dfe53b044eb13a13f273
pratik-prakash-sharma <123077855+pratik-prakash-sharma@users.noreply.github.com>
2025-06-03 03:30:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,6 @@ app.use(session({
 //     // await mongoose.connect("mongodb://localhost:27017/skillswap");
 // }
 
-const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
