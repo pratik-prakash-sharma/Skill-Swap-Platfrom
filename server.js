938290d753cc0f3c5c8915629b1babfa2462a847
pratik-prakash-sharma <pratikprakashsharma@gmail.com>
2025-06-03 05:07:16 +0530

    update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 //     const skills = await Skill.find();
 //     res.render("home.ejs", {skills});
 // })
-app.get("/home", async (req, res) => {
+app.get("/", async (req, res) => {
     // const skills = await Skill.find();
     // if (!req.session.user) return res.redirect("/user");
 
